9fdc3d8615c214bc314f31139dd2a9a16135b98a
Laputa <Laputa1729@163.com>
2023-05-28 16:16:14 +0800

    Chores.

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,31 @@
 <template>
-  <h1>一个人的信息：</h1>
-  <h2>姓名：{{ name }}；年龄：{{ age }}</h2>
-  <h3>工作：{{ job.type }}</h3>
-  <h3>薪水：{{ job.salary }}</h3>
-  <h3>c：{{ job.a.b.c }}</h3>
-  <h3>爱好：{{ hobbies }}</h3>
-  <button @click="changeInfo">修改</button>
+  <Demo msg="你好哇！" school="Laputa" @hello="showHelloMsg">
+    <span>周杰伦</span>
+
+    <template v-slot:qwe>
+      <span>半岛铁盒</span>
+    </template>
+
+    <template v-slot:asd>
+      <span>东风破</span>
+    </template>
+  </Demo>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import Demo from '@/components/Demo.vue'
 
 export default {
   name: 'App',
+  components: { Demo },
+
   setup() {
-    // ref() 定义响应式数据（多用于基本数据类型）
-    let name = ref('张三');  // => RefImpl {...value} 引用实现对象
-    let age = ref(18);
-
-    // reactive() 定义响应式数据（引用类型）
-    let job = reactive({  // => Proxy {...} 实例对象
-      type: 'Laputa',
-      salary: '100k',
-      a: {
-        b: {
-          c: 666,
-        }
-      }
-    });
-    let hobbies = reactive(['抽烟', '喝酒', '烫头']);
-
-    function changeInfo() {
-      name.value = '李四';
-      age.value = 48;
-
-      console.log(job);
-      job.type = 'UI设计师';
-      job.salary = '666k';
-
-      job.a.b.c = 999;
-      hobbies[0] = '造反';
+    function showHelloMsg(data) {
+      alert(`触发了 hello 事件~~~收到数据：${ data }`);
     }
 
     return {
-      name,
-      age,
-      job,
-      hobbies,
-      changeInfo,
+      showHelloMsg,
     }
   }
 }
---
+++ src/components/Demo.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年龄：{{ person.age }}</h2>
+  <button @click="test1">点击触发 hello 事件</button>
+  <hr>
+  <div><slot></slot></div>
+  <div><slot name="qwe"></slot></div>
+  <div><slot name="asd"></slot></div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  name: 'Demo',
+  props: ['msg', 'school'],
+  emits: ['hello'],
+
+  setup(props, context) {
+    console.log('---setup---', props, context);
+    // 数据
+    let person = reactive({
+      name: '张三',
+      age: 18,
+    });
+
+    function test1() {
+      context.emit('hello', 666);
+    }
+
+    return {
+      person,
+      test1,
+    }
+  }
+}
+</script>
